4d8527fc1b921ce2753c9b3c63c38baf8b9a2f1f
chupei <e06084@126.com>
2024-12-10 20:05:07 +0800

    x

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+
+def parse_requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+
+    requires = []
+
+    for line in lines:
+        if 'http' in line:
+            pkg_name_without_url = line.split('@')[0].strip()
+            requires.append(pkg_name_without_url)
+        else:
+            requires.append(line)
+
+    return requires
+
+
+setup(
+    name='llm_web_kit',
+    version='0.1.0',  # 版本号
+    description='LLM Web Kit for processing and managing web content',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=parse_requirements('requirements.txt'),  # 项目依赖的第三方库
+    extras_require={
+        'dev': [
+            'pytest',
+            'flake8',
+            'isort',
+            'yapf',
+            'pre-commit',
+        ],
+    },
+    python_requires='>=3.10',
+)
