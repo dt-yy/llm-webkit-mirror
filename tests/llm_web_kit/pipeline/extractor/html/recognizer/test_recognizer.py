f340fe21608e68e61bc1296128632092c95844af
drunkpig <60862764+drunkpig@users.noreply.github.com>
2024-12-27 19:59:11 +0800

    test: add test case (#40)
    * test: add test case
    
    * test: add test case
    
    ---------
    
    Co-authored-by: xuchao <xuchao@pjlab.org.cn>

--- tests/llm_web_kit/pipeline/extractor/html/recognizer/test_recognizer.py
+++ tests/llm_web_kit/pipeline/extractor/html/recognizer/test_recognizer.py
@@ -29,11 +29,17 @@ class TestBaseHTMLElementRecognizer(unittest.TestCase):
         assert result[0][1] == '$E=MC^2$'
 
     def test_html_split_by_tags_with_parent_nodes(self):
+        """
+        测试是否能够正确带上父节点
+        """
         with open(f'{os.path.dirname(os.path.abspath(__file__))}/assets/recognizer/with_parent_nodes.html', 'r') as file:
             html_content = file.read()
 
         result_with_parent = BaseHTMLElementRecognizer.html_split_by_tags(html_content, 'cccode', parent=True)
         assert len(result_with_parent) == 10
+        assert result_with_parent[0][0] == "<html><body><article>这里是text</article></body></html>"
+        assert result_with_parent[3][0] == '<html><body><article><cccode>print("BBBBBB")</cccode></article></body></html>'
+        assert result_with_parent[9][0] == '<html><body><article><div><p>这里是tail2</p></div></article></body></html>'
 
         result = BaseHTMLElementRecognizer.html_split_by_tags(html_content, 'cccode')
         assert len(result) == 10
