a725b6fa6f38da9e47020d0ac053d4ff87fd3f63
quyuan <quyuan@pjlab.org>
2025-01-22 14:09:16 +0800

    add update version

--- update_version.py
+++ update_version.py
@@ -3,17 +3,17 @@ import subprocess
 
 
 def get_version():
-    command = ["git", "describe", "--tags"]
+    command = ['git', 'describe', '--tags']
     try:
         version = subprocess.check_output(command).decode().strip()
-        version_parts = version.split("-")
-        if len(version_parts) > 1 and version_parts[0].startswith("magic_pdf"):
+        version_parts = version.split('-')
+        if len(version_parts) > 1 and version_parts[0].startswith('magic_pdf'):
             return version_parts[1]
         else:
-            raise ValueError(f"Invalid version tag {version}. Expected format is magic_pdf-<version>-released.")
+            raise ValueError(f'Invalid version tag {version}. Expected format is magic_pdf-<version>-released.')
     except Exception as e:
         print(e)
-        return "0.0.0"
+        return '0.0.0'
 
 
 def write_version_to_commons(version):
