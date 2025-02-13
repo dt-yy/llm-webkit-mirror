983ad5f7df0c6775894f09561479a5c3d84f7576
quyuan <quyuan@pjlab.org>
2025-02-13 20:35:09 +0800

    update jsonc

--- llm_web_kit/pipeline/extractor/html/pre_extractor.py
+++ llm_web_kit/pipeline/extractor/html/pre_extractor.py
@@ -57,34 +57,6 @@ class HTMLFileFormatFilterTablePreExtractor(HTMLFileFormatFilterPreExtractor):
             return False
 
 
-class TestHTMLFileFormatFilterTablePreExtractor(HTMLFileFormatFilterTablePreExtractor):
-    """为了方便对测试数据进行测试，需要吧测试数据的格式转换为处理HTML数据的标准的DataJson格式
-    也就是测试数据的html以文件放在磁盘路径下，但是标准的DataJson格式是html以字符串的形式存在于jsonl中的html字段里。
-    这个类就是根据路径读取html文件，然后转换为DataJson格式。"""
-
-    def __init__(self, config: dict, html_parent_dir: str):
-        """
-        初始化函数
-        Args:
-            config:
-            html_parent_dir:
-        """
-        super().__init__(config)
-        self.__html_parent_path = html_parent_dir
-
-    @override
-    def _do_pre_extract(self, data_json: DataJson) -> DataJson:
-        """对输入的单个html拼装到DataJson中，形成标准输入格式."""
-        proj_root_dir = get_proj_root_dir()
-        html_file_path = os.path.join(proj_root_dir, self.__html_parent_path, data_json.get('path'))
-
-        with open(html_file_path, 'r', encoding='utf-8') as f:
-            html = f.read()
-            data_json['html'] = html
-            del data_json['path']
-        return data_json
-
-
 class TestHTMLFileFormatFilterPreExtractor(HTMLFileFormatFilterPreExtractor):
     """为了方便对测试数据进行测试，需要吧测试数据的格式转换为处理HTML数据的标准的DataJson格式
     也就是测试数据的html以文件放在磁盘路径下，但是标准的DataJson格式是html以字符串的形式存在于jsonl中的html字段里。
