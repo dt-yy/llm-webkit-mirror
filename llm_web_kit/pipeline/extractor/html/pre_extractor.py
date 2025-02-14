ab802d25ec6500b16e62f57e52532d0f4c5c6c8e
yyy <102640628+dt-yy@users.noreply.github.com>
2025-02-14 20:27:39 +0800

    add remove format table (#178)
    Co-authored-by: quyuan <quyuan@pjlab.org>

--- llm_web_kit/pipeline/extractor/html/pre_extractor.py
+++ llm_web_kit/pipeline/extractor/html/pre_extractor.py
@@ -3,6 +3,7 @@ import os
 from overrides import override
 
 from llm_web_kit.input.datajson import DataJson
+from llm_web_kit.libs.html_utils import html_to_element
 from llm_web_kit.libs.path_lib import get_proj_root_dir
 from llm_web_kit.pipeline.extractor.pre_extractor import \
     BaseFileFormatFilterPreExtractor
@@ -28,11 +29,40 @@ class HTMLFileFormatFilterPreExtractor(BaseFileFormatFilterPreExtractor):
         return data_json
 
 
+class HTMLFileFormatFilterTablePreExtractor(HTMLFileFormatFilterPreExtractor):
+    def __init__(self, config: dict):
+        super().__init__(config)
+
+    @override
+    def _filter_by_rule(self, data_json: DataJson) -> bool:
+        if self.__remove_format_table(data_json):
+            return True
+        else:
+            return False
+
+    @override
+    def _do_pre_extract(self, data_json: DataJson) -> DataJson:
+        pass  # TODO
+        return data_json
+
+    def __remove_format_table(self, data_json: DataJson):
+        """remove 排版table."""
+        html_content = data_json['html']
+        html_str = html_to_element(html_content)
+        first_structure = html_str.xpath('/html/body/table') != []
+        second_structure = html_str.xpath('/html/body/center/table') != []
+        if bool(first_structure and second_structure):
+            return True
+        else:
+            return False
+
+
 class TestHTMLFileFormatFilterPreExtractor(HTMLFileFormatFilterPreExtractor):
     """为了方便对测试数据进行测试，需要吧测试数据的格式转换为处理HTML数据的标准的DataJson格式
     也就是测试数据的html以文件放在磁盘路径下，但是标准的DataJson格式是html以字符串的形式存在于jsonl中的html字段里。
     这个类就是根据路径读取html文件，然后转换为DataJson格式。"""
-    def __init__(self, config: dict, html_parent_dir:str):
+
+    def __init__(self, config: dict, html_parent_dir: str):
         """
         初始化函数
         Args:
