f4c707c1e349c9c6832bdbafc7cb63083230b5e6
quyuan <quyuan@pjlab.org>
2025-02-19 16:14:09 +0800

    add remove empty table

--- tests/llm_web_kit/pipeline/extractor/html/recognizer/test_table.py
+++ tests/llm_web_kit/pipeline/extractor/html/recognizer/test_table.py
@@ -19,7 +19,8 @@ TEST_CASES = [
             'assets/recognizer/table_include_image.html',
             'assets/recognizer/table_simple_cc.html',
             'assets/recognizer/table_include_rowspan_colspan.html',
-            'assets/recognizer/table_involve_equation.html'
+            'assets/recognizer/table_involve_equation.html',
+            "assets/recognizer/table_include_code.html"
 
         ),
         'expected': [
@@ -152,3 +153,14 @@ class TestTableRecognizer(unittest.TestCase):
             raw_html = raw_html_path.read_text(encoding='utf-8')
             parts = self.rec.recognize(base_url, [(raw_html, raw_html)], raw_html)
             assert parts is not None
+
+    def test_table_involve_code(self):
+        """
+        test table involve code, code被提取出去了
+        """
+        for test_case in TEST_CASES:
+            raw_html_path = base_dir.joinpath(test_case['input'][10])
+            base_url = 'https://en.m.wikipedia.org/wiki/Variance'
+            raw_html = raw_html_path.read_text(encoding='utf-8')
+            parts = self.rec.recognize(base_url, [(raw_html, raw_html)], raw_html)
+            assert len(parts) == 0
