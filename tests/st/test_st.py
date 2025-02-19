cf19662f8cd945744a70f2543a0d568289c37c04
chupei <e06084@126.com>
2025-02-19 19:35:33 +0800

    x

--- tests/st/test_st.py
+++ tests/st/test_st.py
@@ -1,5 +1,8 @@
+import json
+import os
 import sys
 import unittest
+import uuid
 from pathlib import Path
 
 
@@ -9,21 +12,59 @@ class TestST(unittest.TestCase):
     def setUp(self):
         """设置测试环境."""
         # 获取项目根目录的绝对路径
-        root_path = Path(__file__).parent.parent.parent.absolute()
-        print('rootPath: ', root_path)
+        self.root = Path(__file__).parent.parent.parent.absolute()
 
         # 将项目根目录添加到Python路径
-        if str(root_path) not in sys.path:
-            sys.path.insert(0, str(root_path))
+        if str(self.root) not in sys.path:
+            sys.path.insert(0, str(self.root))
+
+        self.sourcePath = os.path.join(self.root, 'bench/data/all.json')
+        self.outputPath = os.path.join(self.root, 'bench/output')
+        self.pipelineConfigPath = os.path.join(self.root, 'bench/config/ours_config.jsonc')
+        self.pipeline_data_path = os.path.join(self.root, 'bench/config/ours_data_config.jsonl')
 
     def test_st_bench(self):
         """测试run.py."""
-        from bench.run import main
+        from bench.common.result import (Error_Item, Result_Detail,
+                                         Result_Summary)
+        from bench.eval.ours import eval_ours_extract_html
 
-        summary, detail = main()
+        task_id = str(uuid.uuid1())
+        output_path = os.path.join(self.outputPath, f'{task_id}')
+
+        summary = Result_Summary.create(
+            task_id=task_id,
+            output_path=output_path,
+            total=0,
+            result_summary={},
+            error_count=0
+        )
+
+        # 创建评测结果详情
+        detail = Result_Detail.create(
+            task_id=summary.task_id,  # 使用相同的task_id
+            output_path=output_path,
+        )
+
+        with open(self.sourcePath, 'r') as f:
+            files = json.load(f)
+            # files结构是{"filename":{"url":"","filepath":""}}，获取filepath
+            for fileName in files:
+                filepath = files[fileName]['origin_filepath']
+                summary.total += 1
+                print(f'开始抽取:{filepath}...')
+                try:
+                    output, content_list, main_html, statics = eval_ours_extract_html(self.pipelineConfigPath, self.pipeline_data_path, f'{self.root}/bench/data/{filepath}')
+                except Exception as e:
+                    summary.error_summary['count'] += 1
+                    detail.result_detail['error_result'].append(Error_Item(
+                        file_path=filepath,
+                        error_detail=str(e)
+                    ))
+
         self.assertIsNotNone(summary)
         self.assertIsNotNone(detail)
-        self.assertEqual(summary.error_summary['count'], 0, msg=f'测试数据抽取有失败, 抽取失败的数据详情参考: {detail.to_dict()}')
+        self.assertEqual(summary.error_summary['count'], 0, msg=f'测试数据抽取有失败, 抽取失败的数据详情: {detail.to_dict()}')
 
 
 if __name__ == '__main__':
