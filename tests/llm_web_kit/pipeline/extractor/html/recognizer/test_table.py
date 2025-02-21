847fc76a60a68e6d76ca7c3cb5a53f5ec0732d21
quyuan <quyuan@pjlab.org>
2025-02-21 15:12:16 +0800

    remove empty table

--- tests/llm_web_kit/pipeline/extractor/html/recognizer/test_table.py
+++ tests/llm_web_kit/pipeline/extractor/html/recognizer/test_table.py
@@ -155,7 +155,7 @@ class TestTableRecognizer(unittest.TestCase):
             assert parts is not None
 
     def test_table_involve_code(self):
-        """test table involve code, code被提取出去了，过滤掉空的和坏的table"""
+        """test table involve code, code被提取出去了，过滤掉空的和坏的table."""
         for test_case in TEST_CASES:
             raw_html_path = base_dir.joinpath(test_case['input'][10])
             base_url = 'https://en.m.wikipedia.org/wiki/Variance'
