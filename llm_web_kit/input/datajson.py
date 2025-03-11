83a75c8a5f044bfb702a96f8079190f7334971cb
drunkpig <xuchao@pjlab.org.cn>
2025-03-11 17:35:56 +0800

    fix: datajson.to_dict()

--- llm_web_kit/input/datajson.py
+++ llm_web_kit/input/datajson.py
@@ -473,7 +473,7 @@ class DataJson(StructureChecker):
             str: json字符串
         """
         json_dict = self.__json_data.copy()
-        json_dict[DataJsonKey.CONTENT_LIST] = self.get_content_list()._get_data()
+        json_dict[DataJsonKey.CONTENT_LIST] = self.get_content_list().to_dict()
         if pretty:
             return json.dumps(json_dict, indent=2, ensure_ascii=False)
         return json.dumps(json_dict, ensure_ascii=False)
@@ -485,5 +485,5 @@ class DataJson(StructureChecker):
             dict: dict对象
         """
         json_dict = self.__json_data.copy()
-        json_dict[DataJsonKey.CONTENT_LIST] = self.get_content_list()._get_data()
+        json_dict[DataJsonKey.CONTENT_LIST] = self.get_content_list().to_dict()
         return json_dict
