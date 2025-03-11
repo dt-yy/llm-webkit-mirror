50fafbae2d05249a1ba00e2f9dcf5c4ed245654d
drunkpig <xuchao@pjlab.org.cn>
2025-03-11 15:17:08 +0800

    fix: test error

--- tests/llm_web_kit/libs/test_standard_utils.py
+++ tests/llm_web_kit/libs/test_standard_utils.py
@@ -48,13 +48,13 @@ def test_json_loads(input: Union[str, bytes], target_dict) -> None:
         '0': 'aaa',
         '1': 'bbb',
         '2': 'ccc'
-    }, '''{"0": "aaa", "1": "bbb", "2": "ccc"}'''),
+    }, '''{"0":"aaa","1":"bbb","2":"ccc"}'''),
     ({
         'track_id': '7c5b99d3',
         'warc_record_offset': 65390694,
         'warc_record_length': '16190',
         'layout_id': 0
-    }, '{"track_id": "7c5b99d3", "warc_record_offset": 65390694, "warc_record_length": "16190", "layout_id": 0}'),
+    }, '{"track_id":"7c5b99d3","warc_record_offset":65390694,"warc_record_length":"16190","layout_id":0}'),
 ])
 def test_json_dumps(input_dict: dict, target_str) -> None:
     """
@@ -66,4 +66,10 @@ def test_json_dumps(input_dict: dict, target_str) -> None:
     Returns: None
 
     """
+    expected_obj = json_loads(target_str)
+    # 比较两个对象是否相等
+    for key, value in input_dict.items():
+        assert expected_obj[key] == value
+
+    # 比较json_dumps的输出是否与target_str相等
     assert target_str == json_dumps(input_dict)
