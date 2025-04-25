30ea99fb066655f9a27f75463e338ba45c59d935
dt-yy <qywan918@163.com>
2025-04-25 10:56:18 +0800

    fix pylint

--- tests/llm_web_kit/main_html_parser/parser/test_layout_parser.py
+++ tests/llm_web_kit/main_html_parser/parser/test_layout_parser.py
@@ -1,8 +1,11 @@
 import json
 import unittest
 from pathlib import Path
+
 from llm_web_kit.input.pre_data_json import PreDataJson, PreDataJsonKey
-from llm_web_kit.main_html_parser.parser.layout_batch_parser import LayoutBatchParser
+from llm_web_kit.main_html_parser.parser.layout_batch_parser import \
+    LayoutBatchParser
+
 TEST_CASES = [
     {
         'input': (
@@ -25,11 +28,10 @@ class TestLayoutParser(unittest.TestCase):
             raw_html_path = base_dir.joinpath(test_case['input'][0])
             element_path = base_dir.joinpath(test_case['input'][1])
             raw_html = raw_html_path.read_text()
-            element_json = json.loads(element_path.read_text())  
-            data_dict = {"HTML": raw_html, "TEMPLATE_DATA": element_json, "ORI_HTML": raw_html}
+            element_json = json.loads(element_path.read_text())
+            data_dict = {'HTML': raw_html, 'TEMPLATE_DATA': element_json, 'ORI_HTML': raw_html}
             expected_html = base_dir.joinpath(test_case['expected'][0]).read_text()
             pre_data = PreDataJson(data_dict)
             parser = LayoutBatchParser(element_json)
             parts = parser.parse(pre_data)
             assert parts.get(PreDataJsonKey.MAIN_HTML_BODY) == expected_html
-          
